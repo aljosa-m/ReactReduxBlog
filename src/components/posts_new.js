71b270536965763727045f518f5bb314e280fd2b
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-09-02 23:16:33 +0800

    added form fields with validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+  //Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title at least 3 characters";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content"
+  }
+  //If errors is empty, the form is fine to submit
+  //If errors has any properties, redux form assumes the form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
