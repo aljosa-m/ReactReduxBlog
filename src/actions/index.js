5c841865f88eee693b67b92b0a1bdbfd084f6250
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-09-03 21:14:21 +0800

    completed from with API post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,28 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_post';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=PAPERCLIPP1234'
 
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
