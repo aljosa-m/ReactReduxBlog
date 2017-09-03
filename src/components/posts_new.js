6ebbbf121ffb4ffafbab467a1a4833d29764a573
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-09-03 10:23:13 +0800

    added validation messages with conditional styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,21 +3,34 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field){
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values)
+  }
+
+
   render() {
+    const { handleSubmit } = this.props;
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -33,6 +46,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
@@ -41,8 +55,8 @@ class PostsNew extends Component {
 function validate(values) {
   const errors = {};
   //Validate the inputs from 'values'
-  if (!values.title || values.title.length < 3) {
-    errors.title = "Enter a title at least 3 characters";
+  if (!values.title) {
+    errors.title = "Enter a title";
   }
   if (!values.categories) {
     errors.categories = "Enter some categories";
